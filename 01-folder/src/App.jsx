38867b3ed371d42b8b0b458aa35a9525a6f3f1b2
monika6843 <mishramonika5801@gmail.com>
2026-02-28 12:24:44 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,26 @@
+// function App() {
+//   return <h1>Like krdo!!!</h1>;
+// }
+
+// export default App;
+// //  🚀
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      
+      <h2 id='child1'>monika</h2>
+      <h3 id='child3'>from cse</h3>
+    </div>
+    <div id='chacha'>
+      <h1>mooo</h1>
+      <h2>ffhj</h2>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
